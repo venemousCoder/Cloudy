427503880334fa5bc72da6d14a305a3a2461d6fc
venemousCoder <ahmadgameriv@gmail.com>
2025-06-03 16:01:50 +0100

    feat: Configure Memcached host based on environment
    This commit configures the Memcached host based on the environment.
    - Uses `10.0.0.23:11211` as the Memcached host when the environment is production.
    - Uses `localhost:11211` as the Memcached host when the environment is not production.
    - Sets the environment variable in the `.env` file.

--- utils/cache.js
+++ utils/cache.js
@@ -1,7 +1,8 @@
 // utils/cache.js
 const { promisify } = require("util");
 const Memcached = require("memcached");
-const memcached = new Memcached("localhost:11211");
+const memcachedIp = process.env.ENVIRONMENT === "production"? "10.0.0.23:11211": "localhost:11211"
+const memcached = new Memcached(memcachedIp);
 
 const memGet = promisify(memcached.get).bind(memcached);
 const memSet = promisify(memcached.set).bind(memcached); // adjust host if needed
