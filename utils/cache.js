b538223f2523fdb3865abe7c43730e992701dc60
venemousCoder <ahmadgameriv@gmail.com>
2025-06-05 23:22:26 +0100

    feat: Configure Memcached with username, password, and host from environment variables
    This commit updates the Memcached configuration to use environment variables for the host, username, and password.
    
    - Updates the `.env` file to include `CACHE_USERNAME`, `CACHE_PASSWORD`, and `CACHE_HOST` variables.
    - Modifies the `utils/cache.js` file to read the Memcached host, username, and password from the environment variables.
    - Adds options for retries, retry interval, and remove failed servers to the Memcached client configuration.

--- utils/cache.js
+++ utils/cache.js
@@ -2,7 +2,16 @@
 const { promisify } = require("util");
 const Memcached = require("memcached");
 const memcachedIp = process.env.ENVIRONMENT === "production"? "10.0.0.23:11211": "localhost:11211"
-const memcached = new Memcached(memcachedIp);
+const memcached = new Memcached(
+  process.env.CACHE_HOST, // hostname from your dashboard
+  {
+    username: process.env.CACHE_USERNAME,
+    password: process.env.CACHE_PASSWORD ,
+    retries: 10,
+    retry: 10000,
+    remove: true,
+  }
+);
 
 const memGet = promisify(memcached.get).bind(memcached);
 const memSet = promisify(memcached.set).bind(memcached); // adjust host if needed
